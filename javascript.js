fa5e90df03e476d915b605e198256bd7e2293487
musab2020 <musab2020@hotmail.com>
2018-06-29 01:35:16 +0200

    Java Script Week 1

---
+++ javascript.js
@@ -0,0 +1,62 @@
+console.log('مرحباً بالعالم'); //Arabic Greeting's
+
+console.log('Hello World');//English Greeting's
+
+console.log('Hallo Werld');//Nederlands Groetjes
+
+//Find the solution for the error
+
+console.log('Im awesome'); 
+
+//Declare a variable x and initialize it with an integer
+
+var 
+x =10;
+console.log('The value of the variable x will be: 10');
+console.log(x);
+
+//Declare a variable y and assign a string to it.
+
+//let  y = "SurName";
+console.log('The Value Of My String Will Be: Name');
+//console.log(y);
+let y = "SurName";
+console.log('Khalaf');
+console.log(y);
+
+//How do you round the number 7.25, to the nearest integer (i.e., whole number)?
+
+var z = 7.25;
+console.log(z);
+var a = Math.round(z);
+console.log(a);
+
+var m = "";
+if (z > a)
+{
+var m = z;
+}
+else
+{ var m = a }
+
+console.log(m);
+
+//Array
+
+var EmptyArray = [];
+console.log('Array Has No Thing On It');
+console.log(EmptyArray);
+var FavoriteAnimals = ["Huorse","Wolf","Dolphin"];
+console.log(FavoriteAnimals);
+FavoriteAnimals.push('Baby Pig');
+console.log(FavoriteAnimals);
+
+//More strings
+
+let myString = "Mousab Mohamad It Should be 30";
+console.log(myString);
+console.log(myString.length);
+
+//Write a program that checks the types of two ...
+
+
